0d35f7f331ba897973915c3e1fbb38d4db64aba0
Yash Raj Singh <yashudps.ys@gmail.com>
2024-01-08 15:18:29 +0530

    feat: booking application

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("Hello, World!")
+	fmt.Println("Welcome to our conference booking application")
+	fmt.Println("Get your tickets here to attend")
 
+	var conferenceName = "Go Conference"
+	fmt.Println(conferenceName)
 }
